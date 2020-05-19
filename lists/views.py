bc734af74923ce763e10a687c758efa269221654
osbash <781442542@qq.com>
2020-05-19 11:05:13 +0800

    add list.html

--- lists/views.py
+++ lists/views.py
@@ -6,8 +6,12 @@ from lists.models import Item
 def home_page(request):
 	if request.method == 'POST':
 		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/')
-	items = Item.objects.all()
+		return redirect('/lists/the-only-list-in-the-world/')
+#	items = Item.objects.all()
 #	for item in items:
 #		item.delete()
-	return render(request, 'home.html', {'items':items})
+	return render(request, 'home.html')
+	
+def view_list(request):
+	items = Item.objects.all()
+	return render(request, 'list.html', {'items':items})
